f817db30c6a5d8835e0849f2cd111f09986e3a13
Encryptar <64878818+Encryptar@users.noreply.github.com>
2021-11-17 20:58:19 +1300

    See comments
    Added Recipes and general fleshing out

--- src/main/java/net/Encryptar/IndustrialExpansion/common/blocks/TapBlock.java
+++ src/main/java/net/Encryptar/IndustrialExpansion/common/blocks/TapBlock.java
@@ -3,30 +3,22 @@ package net.Encryptar.IndustrialExpansion.common.blocks;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 
 import javax.annotation.Nullable;
 
-public class TapBlock extends BlockBehaviour {
+public class TapBlock extends DirectionalBlock {
 
     public static final DirectionProperty facing = BlockStateProperties.HORIZONTAL_FACING;
 
     public TapBlock(Properties p) {
         super(p);
-
     }
 
-    @Override
-    public Item asItem() {
-        return null;
-    }
-
-    @Override
-    protected Block asBlock() {
-        return null;
-    }
 
 
 }
